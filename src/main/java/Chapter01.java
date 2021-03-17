4d48f08f13d4bd80d609d7ce408281e01d5fd3c8
wusd <wsd5656126@foxmail.com>
2021-03-17 22:16:35 +0800

    chapter01

--- src/main/java/Chapter01.java
+++ src/main/java/Chapter01.java
@@ -45,12 +45,15 @@ public class Chapter01 {
     }
 
     public String postArticle(Jedis conn, String user, String title, String link) {
+        // 生成新文章ID
         String articleId = String.valueOf(conn.incr("article:"));
 
+        // 将发布人设置已投票,并设置过期时间一周
         String voted = "voted:" + articleId;
         conn.sadd(voted, user);
         conn.expire(voted, ONE_WEEK_IN_SECONDS);
 
+        // 存储文章到散列
         long now = System.currentTimeMillis() / 1000;
         String article = "article:" + articleId;
         HashMap<String,String> articleData = new HashMap<String,String>();
@@ -60,21 +63,29 @@ public class Chapter01 {
         articleData.put("now", String.valueOf(now));
         articleData.put("votes", "1");
         conn.hmset(article, articleData);
+        // 设置评分
         conn.zadd("score:", now + VOTE_SCORE, article);
+        // 设置发布时间
         conn.zadd("time:", now, article);
 
         return articleId;
     }
 
     public void articleVote(Jedis conn, String user, String article) {
+        // 计算文章的投票截止时间
         long cutoff = (System.currentTimeMillis() / 1000) - ONE_WEEK_IN_SECONDS;
+        // 检测是否可以投票
         if (conn.zscore("time:", article) < cutoff){
             return;
         }
 
+        // 取出文章id
         String articleId = article.substring(article.indexOf(':') + 1);
+        // 如果第一次投票
         if (conn.sadd("voted:" + articleId, user) == 1) {
+            // 增加分数
             conn.zincrby("score:", VOTE_SCORE, article);
+            // 增加文章评分
             conn.hincrBy(article, "votes", 1);
         }
     }
@@ -84,11 +95,20 @@ public class Chapter01 {
         return getArticles(conn, page, "score:");
     }
 
+    /**
+     * 根据页码获取文章, 按评分排序
+     * @param conn
+     * @param page
+     * @param order
+     * @return
+     */
     public List<Map<String,String>> getArticles(Jedis conn, int page, String order) {
         int start = (page - 1) * ARTICLES_PER_PAGE;
         int end = start + ARTICLES_PER_PAGE - 1;
 
+        // 获取多个文章id
         Set<String> ids = conn.zrevrange(order, start, end);
+        // 再获取文章列表
         List<Map<String,String>> articles = new ArrayList<Map<String,String>>();
         for (String id : ids){
             Map<String,String> articleData = conn.hgetAll(id);
@@ -99,6 +119,12 @@ public class Chapter01 {
         return articles;
     }
 
+    /**
+     * 为文章添加分组(多个)
+     * @param conn
+     * @param articleId
+     * @param toAdd
+     */
     public void addGroups(Jedis conn, String articleId, String[] toAdd) {
         String article = "article:" + articleId;
         for (String group : toAdd) {
@@ -110,11 +136,22 @@ public class Chapter01 {
         return getGroupArticles(conn, group, page, "score:");
     }
 
+    /**
+     * 根据分组名称获取文章列表
+     * @param conn
+     * @param group
+     * @param page
+     * @param order
+     * @return
+     */
     public List<Map<String,String>> getGroupArticles(Jedis conn, String group, int page, String order) {
+        // 存储zinterstore的结果
         String key = order + group;
+        // 没有,就交集出结果
         if (!conn.exists(key)) {
             ZParams params = new ZParams().aggregate(ZParams.Aggregate.MAX);
             conn.zinterstore(key, params, "group:" + group, order);
+            // 缓存60s
             conn.expire(key, 60);
         }
         return getArticles(conn, page, key);
